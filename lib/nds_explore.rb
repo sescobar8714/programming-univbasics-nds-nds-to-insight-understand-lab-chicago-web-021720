c004719c4156c165899f9ace1636cdb910284bdd
Samuel Escobar <sescobar8714@gmail.com>
2020-02-05 22:25:07 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -15,7 +15,7 @@ def print_first_directors_movie_titles
   movies = directors_database
   index = 0
   while index < movies.count
-    pp title[0][:movies] == "Steven Spielberg"
+    pp movies[0][:title] == "Steven Spielberg"
     index += 1
   end
 
